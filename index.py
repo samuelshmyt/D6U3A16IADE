a857e79df998fb82cdaeb594c9dcaae7c26a33b8
SAMUELSHMYT <samuelshmyt@hotmail.com>
2024-09-15 10:48:09 -0300

    Update index.py
    conexção

--- index.py
+++ index.py
@@ -84,5 +84,5 @@ def update_hist(local, square_size, color_map):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    # app.run_server(host="0.0.0.0", port=8050)
+    # app.run_server(debug=True)
+     app.run_server(host="0.0.0.0", port=8050)
